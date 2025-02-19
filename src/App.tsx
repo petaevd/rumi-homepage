ad17921e387d438e28e80ae9fbe97072e530b3c5
Даниил <Daneel1121@yandex.ru>
2025-02-19 18:41:20 +0300

    Updated index.html + added yandex metrica

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from 'pages/HomePage/index.ts';
+import { YMInitializer } from 'react-yandex-metrika';
 
 const App = () => {
+  const YametricaId = import.meta.env.VITE_YAMETRICA_ID;
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      <YMInitializer accounts={[YametricaId]} options={{webvisor: true}} />
+    </div>
   );
 };
 
